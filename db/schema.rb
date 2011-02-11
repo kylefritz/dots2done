e564d2178348d770e51605eca90a0bb0f6495265
Kyle Fritz <kyle.p.fritz@gmail.com>
2011-02-11 10:46:34 -0500

    added seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110211053350) do
+ActiveRecord::Schema.define(:version => 20110211152342) do
+
+  create_table "tasks", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "user_id"
+    t.integer  "days_total"
+    t.integer  "days_complete"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+User.delete_all
+
+kyle = User.create({:name=>"Kyle Fritz", :email=>"kyle.p.fritz@gmail.com"})
+nick = User.create({:name=>"Nick Crocker",:email=>"nicholascrocker@gmail.com"})
+
+Task.delete_all
+
+t1=Task.create({:name=>"learn chinese", :description=>"if china is taking over the world, i better learn some to fit in", :days_total=> 18, :days_complete=>6})
+t2=Task.create({:name=>"water seedlings", :description=>"i just started some seedlings, i hope they don't die", :days_total=> 4, :days_complete=>4})
+kyle.tasks<<[t1,t2]
+
+t3=Task.create({:name=>"drink a glass of water", :description=>"it's good for your health and more acceptable than beer", :days_total=> 7, :days_complete=>0})
+t4=Task.create({:name=>"go to the gym", :description=>"picks up chicks", :days_total=> 14, :days_complete=>10})
+t5=Task.create({:name=>"eat dinner at home", :description=>"it's fun to eat out but i'll never learn to cook this way", :days_total=> 1, :days_complete=>7})
+nick.tasks<<[t3,t4,t5]
+
+
